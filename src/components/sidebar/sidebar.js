4c1baf3c2afb1d1eebe4c5168847be0eac02f233
Trevor French <trevorf96@gmail.com>
2022-08-04 21:06:45 -0600

    building structure

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import logo from "./DATA.svg"
 
 import "./sidebar.css";
 
@@ -8,8 +9,15 @@ export default class Sidebar extends React.Component {
   render() {
     return (
       <div className="sidebar">
+        <img className="sidebar-logo" src={logo} alt="Logo" />
         <div className="sidebar-welcome">{this.props.value}</div>
         <div className="sidebar-item">Profile</div>
+        <div className="sidebar-item"></div>
+        <div className="sidebar-item"></div>
+        <div className="sidebar-item"></div>
+        <div className="sidebar-item"></div>
+        <div className="sidebar-item"></div>
+        <div className="sidebar-item"></div>
         <div className="sidebar-item"></div>
         <div className="sidebar-item"></div>
       </div>
